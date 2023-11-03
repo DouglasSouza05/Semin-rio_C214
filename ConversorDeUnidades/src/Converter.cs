ce432c3051b59844ebdb7c003595e862b72b146f
DouglasSouza05 <douglasbsouza01@gmail.com>
2023-11-03 00:06:06 -0300

    Criando main.cs e Converter.cs com codigos iniciais

---
+++ ConversorDeUnidades/src/Converter.cs
@@ -0,0 +1,7 @@
+public class UnitsConverter
+{
+    public double CelsiusToFahrenheit(double celsius)
+    {
+    return ((celsius * 9) / 5) + 32;
+    }
+}
---
+++ ConversorDeUnidades/src/Main.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Runtime.InteropServices;
+using System.Xml.Serialization;
+
+class Program
+{
+    static void Main()
+    {
+
+        UnitsConverter converter = new UnitsConverter();
+
+        Console.WriteLine("Conversão de Celsius para Fahrenheit");
+        Console.WriteLine("Digite a temperatura em Celsius: ");
+        
+        if (double.TryParse(Console.ReadLine(), out double celsius))
+        {
+            double fahrenheit = converter.CelsiusToFahrenheit(celsius);
+            Console.WriteLine($"Temperatura em Fahrenheit: {fahrenheit:F2} Graus");
+        }
+        else
+        {
+            Console.WriteLine("Entrada inválida. Certifique-se de digitar um número.");
+        }
+    }
+}
