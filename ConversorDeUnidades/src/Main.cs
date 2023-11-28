b745101b8f466bc74ac223c05ed1d6c4b4b4f12e
Eduardo Resende <89862448+EduCR03@users.noreply.github.com>
2023-11-28 20:07:45 -0300

    Adicionando script padrao de conexao com banco de dados porem sem variaveis ou informacoes

--- ConversorDeUnidades/src/Main.cs
+++ ConversorDeUnidades/src/Main.cs
@@ -1,28 +1,65 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using System;
-using System.Runtime.InteropServices;
-using System.Xml.Serialization;
-using Core.Aplication;
+﻿using System;
+using Npgsql;
 
 class Program
 {
     static void Main()
     {
+        // Substitua as variáveis abaixo com suas informações
+        string host = "localhost"; // ou o IP do seu contêiner Docker
+        string database = "seu_banco_de_dados";
+        string username = "seu_usuario";
+        string password = "sua_senha";
+        int port = 5432; // Porta padrão do PostgreSQL
 
-        // UnitsConverter unitsConverter = new UnitsConverter();
+        // String de conexão
+        string connString = $"Host={host};Username={username};Password={password};Database={database};Port={port}";
 
-        // Console.WriteLine("Conversão de Celsius para Fahrenheit");
-        // Console.WriteLine("Digite a temperatura em Celsius: ");
-        
-        // if (double.TryParse(Console.ReadLine(), out double celsius))
-        // {
-        //     double fahrenheit = unitsConverter.CelsiusToFahrenheit(celsius);
-        //     Console.WriteLine($"Temperatura em Fahrenheit: {fahrenheit:F2} Graus");
-        // }
-        // else
-        // {
-        //     Console.WriteLine("Entrada inválida. Certifique-se de digitar um número.");
-        // }
+        // Instância da conexão
+        using (NpgsqlConnection conn = new NpgsqlConnection(connString))
+        {
+            conn.Open(); // Abre a conexão
+
+            // Insira os dados de exemplo no banco de dados
+            InserirDados(conn, "Metro", "Polegada", 2.54, 5.08); // Exemplo de conversão de 2.54 metros para polegadas
+
+            // Exiba os dados do banco de dados
+            MostrarDados(conn);
+        }
+    }
+
+    // Método para inserir dados
+    static void InserirDados(NpgsqlConnection conn, string medida1, string medida2, double valor, double resultado)
+    {
+        using (NpgsqlCommand cmd = new NpgsqlCommand())
+        {
+            cmd.Connection = conn;
+            cmd.CommandText = "INSERT INTO sua_tabela (medida1, medida2, valor, resultado) VALUES (@medida1, @medida2, @valor, @resultado)";
+
+            // Parâmetros
+            cmd.Parameters.AddWithValue("medida1", medida1);
+            cmd.Parameters.AddWithValue("medida2", medida2);
+            cmd.Parameters.AddWithValue("valor", valor);
+            cmd.Parameters.AddWithValue("resultado", resultado);
+
+            // Executa a consulta
+            cmd.ExecuteNonQuery();
+        }
+    }
+
+    // Método para exibir dados
+    static void MostrarDados(NpgsqlConnection conn)
+    {
+        using (NpgsqlCommand cmd = new NpgsqlCommand("SELECT * FROM sua_tabela", conn))
+        {
+            using (NpgsqlDataReader reader = cmd.ExecuteReader())
+            {
+                Console.WriteLine("Medida1\tMedida2\tValor\tResultado");
+                while (reader.Read())
+                {
+                    Console.WriteLine($"{reader.GetString(1)}\t{reader.GetString(2)}\t{reader.GetDouble(3)}\t{reader.GetDouble(4)}");
+                }
+            }
+        }
     }
 }
