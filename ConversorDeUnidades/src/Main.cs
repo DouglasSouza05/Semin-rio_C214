75b4ae0b2b0c2936c679ac868a8cead50ff11708
Eduardo Resende <89862448+EduCR03@users.noreply.github.com>
2023-12-02 15:03:10 -0300

    Mudando a forma de conecatar com o bd

--- ConversorDeUnidades/src/Main.cs
+++ ConversorDeUnidades/src/Main.cs
@@ -1,73 +1,57 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using MongoDB.Driver;
 
-class Program
+public class Startup
 {
-    static void Main()
+    public void ConfigureServices(IServiceCollection services)
     {
-        // Defina as credenciais
-        var username = "root";
-        var password = "root";
-        var databaseName = "BANCO";
-        // Substitua as variáveis abaixo com suas informações admin:pass
-        string connectionString = "mongodb://mongodb:27017"; // ou o endereço do seu contêiner Docker MongoDB
-        // string databaseName = "BANCO_PADRAO";
-        string collectionName = "conversoes";
-
-        var credential = MongoCredential.CreateCredential(databaseName, username, password);
-
-
-        var settings = new MongoClientSettings
+        services.AddSingleton<IMongoClient>(new MongoClient("mongodb://mongodb:27017"));
+        services.AddScoped<IMongoDatabase>(provider =>
         {
-            Credential = credential,
-            Server = new MongoServerAddress("mongobd", 27017)
-        };
+            var client = provider.GetService<IMongoClient>();
+            return client.GetDatabase("seu_banco_de_dados");
+        });
 
-         var client = new MongoClient(settings);
-
-        // Obter referência do banco de dados e coleção
-        var database = client.GetDatabase(databaseName);
-        var collection = database.GetCollection<Conversao>(collectionName);
-
-        // Insira os dados de exemplo no banco de dados
-        InserirDados(collection, "Metro", "Polegada", 2.54, 5.08); // Exemplo de conversão de 2.54 metros para polegadas
-
-        // Exiba os dados do banco de dados
-        MostrarDados(collection);
+        services.AddControllers();
     }
 
-    // Método para inserir dados
-    static void InserirDados(IMongoCollection<Conversao> collection, string medida1, string medida2, double valor, double resultado)
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
-        var conversao = new Conversao
+        if (env.IsDevelopment())
         {
-            Medida1 = medida1,
-            Medida2 = medida2,
-            Valor = valor,
-            Resultado = resultado
-        };
-
-        collection.InsertOne(conversao);
-    }
-
-    // Método para exibir dados
-    static void MostrarDados(IMongoCollection<Conversao> collection)
-    {
-        var conversoes = collection.Find(_ => true).ToList();
-
-        Console.WriteLine("Medida1\tMedida2\tValor\tResultado");
-        foreach (var conversao in conversoes)
-        {
-            Console.WriteLine($"{conversao.Medida1}\t{conversao.Medida2}\t{conversao.Valor}\t{conversao.Resultado}");
+            app.UseDeveloperExceptionPage();
         }
+
+        app.UseRouting();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+            endpoints.MapGet("/", async context =>
+            {
+                var db = context.RequestServices.GetService<IMongoDatabase>();
+                // Use o banco de dados MongoDB (db) conforme necessário...
+                await context.Response.WriteAsync("Hello, World!");
+            });
+        });
     }
 }
 
-// Definição de classe para representar os dados de conversão
-class Conversao
+public class Program
 {
-    public string Medida1 { get; set; }
-    public string Medida2 { get; set; }
-    public double Valor { get; set; }
-    public double Resultado { get; set; }
+    public static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
 }
