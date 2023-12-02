95e9e7712beef8fcb78f3e3cf01765d4038d8047
Eduardo Resende <89862448+EduCR03@users.noreply.github.com>
2023-12-02 15:06:48 -0300

    Mudando a forma de se conexatar ao bd

--- ConversorDeUnidades/src/Main.cs
+++ ConversorDeUnidades/src/Main.cs
@@ -1,57 +1,64 @@
-﻿using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+﻿using System;
 using MongoDB.Driver;
 
-public class Startup
+class Program
 {
-    public void ConfigureServices(IServiceCollection services)
+    static void Main()
     {
-        services.AddSingleton<IMongoClient>(new MongoClient("mongodb://mongodb:27017"));
-        services.AddScoped<IMongoDatabase>(provider =>
-        {
-            var client = provider.GetService<IMongoClient>();
-            return client.GetDatabase("seu_banco_de_dados");
-        });
+        // Defina as credenciais
+        var username = "root";
+        var password = "root";
+        var databaseName = "admin";
+        // Substitua as variáveis abaixo com suas informações admin:pass
+        const string connectionString = "mongodb://root:root@mongodb-1:27017/admin"; // ou o endereço do seu contêiner Docker MongoDB
+        string collectionName = "conversoes";
 
-        services.AddControllers();
+        // Criar cliente MongoDB
+        var client = new MongoClient(connectionString);
+
+        // Obter referência do banco de dados e coleção
+        // var database = client.GetDatabase(databaseName);
+        // var collection = database.GetCollection<Conversao>(collectionName);
+
+        // Insira os dados de exemplo no banco de dados
+        // InserirDados(collection, "Metro", "Polegada", 2.54, 5.08); // Exemplo de conversão de 2.54 metros para polegadas
+
+        // Exiba os dados do banco de dados
+        // MostrarDados(collection);
     }
 
-    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-    {
-        if (env.IsDevelopment())
-        {
-            app.UseDeveloperExceptionPage();
-        }
+    // // Método para inserir dados
+    // static void InserirDados(IMongoCollection<Conversao> collection, string medida1, string medida2, double valor, double resultado)
+    // {
+    //     var conversao = new Conversao
+    //     {
+    //         Medida1 = medida1,
+    //         Medida2 = medida2,
+    //         Valor = valor,
+    //         Resultado = resultado
+    //     };
 
-        app.UseRouting();
+    //     collection.InsertOne(conversao);
+    // }
 
-        app.UseEndpoints(endpoints =>
-        {
-            endpoints.MapControllers();
-            endpoints.MapGet("/", async context =>
-            {
-                var db = context.RequestServices.GetService<IMongoDatabase>();
-                // Use o banco de dados MongoDB (db) conforme necessário...
-                await context.Response.WriteAsync("Hello, World!");
-            });
-        });
-    }
+    // Método para exibir dados
+    // static void MostrarDados(IMongoCollection<Conversao> collection)
+    // {
+    //     var conversoes = collection.Find(_ => true).ToList();
+
+    //     Console.WriteLine("Medida1\tMedida2\tValor\tResultado");
+    //     foreach (var conversao in conversoes)
+    //     {
+    //         Console.WriteLine($"{conversao.Medida1}\t{conversao.Medida2}\t{conversao.Valor}\t{conversao.Resultado}");
+    //     }
+    // }
 }
 
-public class Program
-{
-    public static void Main(string[] args)
-    {
-        CreateHostBuilder(args).Build().Run();
-    }
-
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseStartup<Startup>();
-            });
-}
+// Definição de classe para representar os dados de conversão
+// class Conversao
+// {
+//     public string Medida1 { get; set; }
+//     public string Medida2 { get; set; }
+//     public double Valor { get; set; }
+//     public double Resultado { get; set; }
+// }
