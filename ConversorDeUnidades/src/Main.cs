cd6b9b099d1fe88f7a5361bcc753e6fd8eb87ae3
Eduardo Resende <89862448+EduCR03@users.noreply.github.com>
2023-12-04 18:12:25 -0300

    Colocando na main a forma de conectar por ip

--- ConversorDeUnidades/src/Main.cs
+++ ConversorDeUnidades/src/Main.cs
@@ -1,68 +1,32 @@
 using System;
-using MongoDB.Bson;
-using MongoDB.Bson.Serialization.Attributes;
-using MongoDB.Driver;
-
+using System.Threading.Tasks;
 
 class Program
 {
-    static void Main()
+    static async Task Main(string[] args)
     {
-        var databaseName = "admin";
-        // Substitua as variáveis abaixo com suas informações admin:pass
-        const string connectionString = "mongodb://test:test@mongodb:27017/admin"; // ou o endereço do seu contêiner Docker MongoDB
-        string collectionName = "conversoes";
+        var conn = new Conn();
 
-        // Criar cliente MongoDB
-        var client = new MongoClient(connectionString);
-
-        // Obter referência do banco de dados e coleção
-        var database = client.GetDatabase(databaseName);
-        var collection = database.GetCollection<Conversao>(collectionName);
-
-        // Insira os dados de exemplo no banco de dados
-        InserirDados(collection, "Metro", "Polegada", 2.54, 5.08); // Exemplo de conversão de 2.54 metros para polegadas
-
-        // Exiba os dados do banco de dados
-        MostrarDados(collection);
-    }
-
-    // Método para inserir dados
-    static void InserirDados(IMongoCollection<Conversao> collection, string medida1, string medida2, double valor, double resultado)
-    {
+        // Exemplo de utilização do método InsertOneAsync
         var conversao = new Conversao
         {
-            Medida1 = medida1,
-            Medida2 = medida2,
-            Valor = valor,
-            Resultado = resultado
+            Unidade = "Metro para Quilômetro",
+            Numero = 100,
+            Resposta = 0.1
         };
 
-        collection.InsertOne(conversao);
-    }
+        await conn.InsertOneAsync(conversao);
 
-    // Método para exibir dados
-    static void MostrarDados(IMongoCollection<Conversao> collection)
-    {
-        var conversoes = collection.Find(_ => true).ToList();
+        // Outras operações podem ser realizadas aqui
 
-        Console.WriteLine("Medida1\tMedida2\tValor\tResultado");
-        foreach (var conversao in conversoes)
-        {
-            Console.WriteLine($"{conversao.Medida1}\t{conversao.Medida2}\t{conversao.Valor}\t{conversao.Resultado}");
-        }
+        // Console.WriteLine("Operações concluídas. Pressione qualquer tecla para sair.");
+        // Console.ReadKey();
     }
 }
 
-// Definição de classe para representar os dados de conversão
-class Conversao
+public class Conversao
 {
-
-    [BsonId]
-    [BsonRepresentation(BsonType.ObjectId)]
-    public string Id { get; set; }
-    public string Medida1 { get; set; }
-    public string Medida2 { get; set; }
-    public double Valor { get; set; }
-    public double Resultado { get; set; }
+    public string Unidade { get; set; }
+    public double Numero { get; set; }
+    public double Resposta { get; set; }
 }
